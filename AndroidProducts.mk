99d3c90b454bf93ed42ce32928d6c40501dc64a9
Zed <31599865+Zed9K@users.noreply.github.com>
2024-06-08 21:49:45 +0300

    Adapt to Sigmadroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/sigma_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    sigma_dumpling-user \
+    sigma_dumpling-userdebug \
+    sigma_dumpling-eng
