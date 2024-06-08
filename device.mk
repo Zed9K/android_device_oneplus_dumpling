919bfd6e19031f0bda5d87253b0b5f38211540a2
Zed <31599865+Zed9K@users.noreply.github.com>
2024-06-08 23:04:52 +0300

    Make it Signed.

--- device.mk
+++ device.mk
@@ -43,3 +43,6 @@ PRODUCT_PACKAGES += \
 
 # Inherit from oneplus msm8998-common
 $(call inherit-product, device/oneplus/msm8998-common/common.mk)
+
+# Sign builds
+-include vendor/lineage-priv/keys/keys.mk
