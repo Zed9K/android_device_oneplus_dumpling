c8b6dbdc336bdc6a90bbeed465088ab7b7a79860
Zed <31599865+Zed9K@users.noreply.github.com>
2025-02-11 10:31:38 +0400

    Adapt to EvoX

--- lineage_dumpling.mk
+++ lineage_dumpling.mk
@@ -31,3 +31,14 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     SystemName=OnePlus5T
 
 TARGET_VENDOR := oneplus
+
+# Support call recording on Google Dialer
+TARGET_SUPPORTS_CALL_RECORDING := true
+# Ship with Blur
+TARGET_ENABLE_BLUR := true
+# Remove MATLOG
+TARGET_INCLUDE_MATLOG := false
+# GApps ( Vanilla )
+WITH_GMS := false
+# BCR
+BUILD_BCR := true
